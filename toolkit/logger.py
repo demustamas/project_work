8ac73f8f7938705509a2cfdf4001df46b7303b63
demustamas <tamas.demus@gmail.com>
2023-05-08 16:46:02 +0000

    fitted to ssh

--- toolkit/logger.py
+++ toolkit/logger.py
@@ -1,7 +1,8 @@
 import logging
 import logging.config
 import colorlog
-from pythonjsonlogger import jsonlogger
+
+# from pythonjsonlogger import jsonlogger
 from pathlib import Path
 import sys
 import gc
@@ -28,15 +29,15 @@ class Logger:
             self.logger = logging.getLogger(self.name)
 
             textlog = Path("./logs/log.txt")
-            jsonlog = Path("./logs/log.json")
+            # jsonlog = Path("./logs/log.json")
 
             console = colorlog.StreamHandler(stream=sys.stderr)
             text_logger = logging.handlers.RotatingFileHandler(
                 filename=textlog, mode="a", maxBytes=1048576, backupCount=5
             )
-            json_logger = logging.handlers.RotatingFileHandler(
-                filename=jsonlog, mode="a", maxBytes=1048576, backupCount=5
-            )
+            # json_logger = logging.handlers.RotatingFileHandler(
+            #     filename=jsonlog, mode="a", maxBytes=1048576, backupCount=5
+            # )
 
             fmt_console = colorlog.ColoredFormatter(
                 "[ {log_color}{levelname:^10s}{reset} ] {message:s}", style="{"
@@ -47,24 +48,24 @@ class Logger:
                 "%(name)-40s:%(lineno)5s:%(funcName)-25s |"
                 "%(message)s"
             )
-            fmt_json_logger = jsonlogger.JsonFormatter(
-                "%(asctime)s"
-                "%(levelname)s"
-                "%(name)s"
-                "%(lineno)s"
-                "%(funcName)s"
-                "%(message)s"
-            )
+            # fmt_json_logger = jsonlogger.JsonFormatter(
+            #     "%(asctime)s"
+            #     "%(levelname)s"
+            #     "%(name)s"
+            #     "%(lineno)s"
+            #     "%(funcName)s"
+            #     "%(message)s"
+            # )
 
             console.addFilter(lambda record: record.levelno >= logging.INFO)
 
             console.setFormatter(fmt_console)
             text_logger.setFormatter(fmt_text_logger)
-            json_logger.setFormatter(fmt_json_logger)
+            # json_logger.setFormatter(fmt_json_logger)
 
             self.logger.addHandler(console)
             self.logger.addHandler(text_logger)
-            self.logger.addHandler(json_logger)
+            # self.logger.addHandler(json_logger)
 
             self.logger.setLevel(logging.DEBUG)
             logging.captureWarnings(True)
